599a4a4c06df715f310c9a492f3519a222df69e0
jpmbrito123 <joaomoreira75@gmail.com>
2023-11-02 14:36:39 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,56 +1,64 @@
-import pickle
-import socket 
-import time
-import threading
-import sys
-
-class Sp:
-    def __init__(self,name):
-        self.name=name
-        self.cache = cache()
-    
-    def setCache(self, cache):
-        self.cache=cache
-
-    def addto_cache(self,line,org,default):
-        self.cache.add_line(line,org,default)
-       
-
-def cliente(Spp,endereco):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    porta = 1050
-    s.bind((endereco, porta ))
-
-    while True:
-        (msg, add) = s.recvfrom(1024)
-
-        threading.Thread(target=Spp.responde, args=(msg,add,s,)).start()
-
-def TTZ(Spp,endereco):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    porta = 1060
-    s.bind((endereco, porta ))
-    s.listen()
-
-    while True:
-        connection, add = s.accept()
-        threading.Thread(target=TZ, args=(Spp,connection,add,)).start()
-        
-
-def TZ(Spp,connection,add):
-    None
+import socket
+from socket import SO_REUSEADDR, SOL_SOCKET
 
 
-#existem duas threads uma para tratar dos clientes e uma para tratar dos pedidos de transferencia de zona
-def main():
-    if len(sys.argv)>2:
-        Spp = Sp(sys.argv[1])
 
-        threading.Thread(target=cliente,args=(Spp,sys.argv[2])).start()
-        threading.Thread(target=TTZ,args=(Spp,sys.argv[2])).start()
+
+def handler_404(client_info, is_big_node, nearest_server):
+    """
+    if is_big_node:
+        # envia um pedido ao servidor mais próximo
+        for i in nearest_server:
+            try:
+
+                # envia pedido
+                break
+            except Exception:
+                continue
+    """
+
+    print(f"404 NOT FOUND.\n{client_info}\n")
+    reply = 'RTSP/1.0 404 NOT_FOUND\nCSeq: ' + '\nSession: ' + str(client_info['session'])
+    conn_socket = (client_info['rtspSocket'])[0]
+    conn_socket.send(reply.encode())
+
+
+def handler_500(client_info):
+    print(f"500 CONNECTION ERROR.\n{client_info}\n")
+    reply = 'RTSP/1.0 500 CONNECTION_ERROR\nCSeq: ' + '\nSession: ' + str(client_info['session'])
+    conn_socket = (client_info['rtspSocket'])[0]
+    conn_socket.send(reply.encode())
+
+
+def stream(streamer_info):
+    # streamer_info = (node_id, port_streaming, is_server, MAX_CONN, file_id, message['nearest_server'])
+    nodes_interested = []
+
+    rtsp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    rtsp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    rtsp_socket.bind((streamer_info[0], streamer_info[1]))
+
+    if streamer_info[2]:
+        print(f"\nServidor à escuta em {streamer_info[0]}: {streamer_info[1]} (streaming)\n")
     else:
-        print("error with name")
+        print(f"\nBig Node à escuta em {streamer_info[0]}: {streamer_info[1]} (streaming)\n")
 
-main()
+    rtsp_socket.listen(streamer_info[3])
+
+    # Receber informação sobre cliente (ip,porta) através da sessão RTSP/TCP
+    while True:
+        client_info = {}
+        try:
+            client_info = {'rtspSocket': rtsp_socket.accept()}
+         #Ver como fazer esta parte   ServerStreamer(client_info, streamer_info[4], nodes_interested, not streamer_info[2], streamer_info[5]).run()
+
+        except Exception as ex:
+            if ex == "404":
+                handler_404(client_info, not streamer_info[2], streamer_info[5])
+            elif ex == "500":
+                handler_500(client_info)
+            else:
+                print(f"Exception: [{ex}]\n")
+            break
+
+    rtsp_socket.close()
